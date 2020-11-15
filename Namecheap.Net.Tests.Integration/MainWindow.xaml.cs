4fd1d9390c8fb3497b9fd6facfcfab6920d1c784
Kyle Buzby <kyle@buzby.dev>
2020-11-15 09:42:11 -0600

    Create starter test view for dynmically creating requests
    TODO:
    - Actually send requests
    - Support sandbox toggle
    - Updates to support multiple of one type
    - Implement more requests

--- Namecheap.Net.Tests.Integration/MainWindow.xaml.cs
+++ Namecheap.Net.Tests.Integration/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,8 +26,8 @@ namespace Namecheap.Net.Tests.Integration
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        private string _apiKey;
-        public string ApiKey
+        private string? _apiKey;
+        public string? ApiKey
         {
             get => _apiKey;
             set
@@ -34,9 +36,19 @@ namespace Namecheap.Net.Tests.Integration
                 OnPropertyChanged();
             }
         }
+        private string? _userName;
+        public string? UserName
+        {
+            get => _userName;
+            set
+            {
+                _userName = value;
+                OnPropertyChanged();
+            }
+        }
 
-        private string _apiResponse;
-        public string ApiResponse {
+        private string? _apiResponse;
+        public string? ApiResponse {
             get => _apiResponse;
             set
             {
@@ -45,13 +57,23 @@ namespace Namecheap.Net.Tests.Integration
             } 
         }
 
+        private ObservableCollection<PropertyDisplayInfo>? _requestProperties;
+        public ObservableCollection<PropertyDisplayInfo>? RequestProperties { 
+            get => _requestProperties;
+            set
+            {
+                _requestProperties = value;
+                OnPropertyChanged();
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
         public void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
@@ -59,6 +81,8 @@ namespace Namecheap.Net.Tests.Integration
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
+            if (ApiKey == null) { return; }
+
             NamecheapApi api = new(ApiKey, "kbuzby", new System.Net.IPAddress(new byte[] { 97, 83, 134, 98 }));
             var getHostsResponse = await api.Domains.Dns.GetHosts("buzby", "dev");
             XmlSerializer xmlSerializer = new(typeof(ApiResponse<DnsCommands.GetHostsResponse>));
@@ -68,6 +92,48 @@ namespace Namecheap.Net.Tests.Integration
             xmlSerializer.Serialize(memStream, getHostsResponse);
 
             ApiResponse = Encoding.UTF8.GetString(memStream.ToArray());
+        }
+
+        private void TreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (e.NewValue is TreeViewItem treeViewItem)
+            {
+                var path = GetItemPath(treeViewItem);
+                var requestType = GetRequestTypeForPath(path);
+                if (requestType == null) { return; }
+                var requestProps = requestType.GetProperties().Where(prop => Attribute.IsDefined(prop, typeof(QueryParamAttribute)));
+                var requestProperties = new ObservableCollection<PropertyDisplayInfo>();
+                foreach (var propInfo in requestProps)
+                {
+                    requestProperties.Add(new PropertyDisplayInfo(propInfo));
+                }
+                RequestProperties = requestProperties;
+            }
+        }
+
+        private static string GetItemPath(TreeViewItem treeViewItem)
+        {
+            return (treeViewItem.Parent is TreeViewItem parentItem ? GetItemPath(parentItem) + "." : "") + treeViewItem.Header;
+        }
+
+        private Type? GetRequestTypeForPath(string itemPath)
+        {
+            return itemPath switch
+            {
+                "Domains.DNS.GetHosts" => typeof(DnsCommands.GetHostsRequest),
+                _ => null
+            };
+        }
+    }
+
+    public record PropertyDisplayInfo
+    {
+        public PropertyInfo PropertyInfo { get; init; }
+        public string PropertyName => PropertyInfo.Name; 
+        public string? PropertyValue { get; init; }
+        public PropertyDisplayInfo(PropertyInfo propertyInfo)
+        {
+            PropertyInfo = propertyInfo;
         }
     }
 }
